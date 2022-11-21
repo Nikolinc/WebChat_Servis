556618810d9a16cc47a3b5aeccd2dbd6b8a793a2
Nikolinc <tsigulskynikita@gmail.com>
2022-11-21 23:22:03 +0500

    add user servise

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { triggerAsyncId } from 'async_hooks';
+import { User } from './users/users.modul';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { Role } from './roles/roles.modul';
+import { UserRoles } from './roles/user-roles.model';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.PROGRESS_HOST,
+      port: Number(process.env.PROGRESS_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [User, Role, UserRoles],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+    RolesModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
